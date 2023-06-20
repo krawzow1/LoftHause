390d9fafb8653d13f356235c505f7fc5a6b31154
Ilya Kravtsov <krawzow1@yandex.ru>
2023-06-20 22:26:31 +0300

    моб версия навигации

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-const navBtn = document.querySelector('nav-icon-btn')
-const navIcon = document.querySelector('nav-icon')
+const navBtn = document.querySelector('.nav-icon-btn')
+const navIcon = document.querySelector('.nav-icon')
+const nav = document.querySelector('.header_top-row')
 
-navBtn.onclic = function() {
-    navIcon.classList.toggle('header_top-row--mobile')
+navBtn.onclick = () => {
+    navIcon.classList.toggle('nav-icon--active')
+    nav.classList.toggle('header_top-row--mobile')
+    document.body.classList.toggle('no-scroll')
 }
